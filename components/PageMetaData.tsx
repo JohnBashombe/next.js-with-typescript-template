8ac061b5168e9e47fef7ed7ff1876eada5fcc3f2
Ntavigwa Bashombe <ntavigwabashombe@gmail.com>
2022-10-25 00:04:04 +0300

    ft(Metadata)

--- components/PageMetaData.tsx
+++ components/PageMetaData.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 const PageMetaData = (): JSX.Element => {
   return (
     <Head>
-      <title>Ntavigwa Template</title>
-      <meta name='description' content='Ensemble Pour Le Changement - RDC' />
+      <title>Next JS Template By Ntavigwa Bashombe</title>
+      <meta name='description' content='Next JS - Template' />
     </Head>
   );
 };
